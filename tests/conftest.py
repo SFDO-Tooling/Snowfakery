ab900cbe36484b3ae1e9e782a8bba00f7c71f912
Paul Prescod <pprescod@salesforce.com>
2020-09-11 14:42:56 -0700

    Cleaner context handling

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,15 +9,13 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 
 @pytest.fixture(scope="function")
 def generated_rows(request):
-    p = patch("snowfakery.output_streams.DebugOutputStream.write_single_row")
-    mockobj = p.start()
-
     def row_values(index, value):
         return mockobj.mock_calls[index][1][1][value]
 
-    mockobj.row_values = row_values
-    yield mockobj
-
-    p.stop()
+    with patch(
+        "snowfakery.output_streams.DebugOutputStream.write_single_row"
+    ) as mockobj:
+        mockobj.row_values = row_values
+        yield mockobj
 
     return
