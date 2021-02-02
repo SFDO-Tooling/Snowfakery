5fab1c21edb47e36e71a4e201f0a740ab8a4a800
Paul Prescod <pprescod@salesforce.com>
2021-02-01 18:43:55 -0800

    Improve test coverage of errors.

--- tests/test_custom_plugins_and_providers.py
+++ tests/test_custom_plugins_and_providers.py
@@ -45,6 +45,8 @@ class WrongTypePlugin(SnowfakeryPlugin):
             "Evaluates its argument twice into a string"
             return int  # function
 
+        junk = None
+
 
 class MyEvaluator(PluginResult):
     def __init__(self, operator, *operands):
@@ -266,6 +268,30 @@ class TestContextVars:
           fields:                               #4
             foo:                                #5
                 WrongTypePlugin.return_bad_type: 5  #6
+        """
+        with pytest.raises(DataGenError) as e:
+            generate(StringIO(yaml))
+        assert 6 > e.value.line_num >= 3
+
+    def test_missing_attributes(self):
+        yaml = """
+        - plugin: tests.test_custom_plugins_and_providers.WrongTypePlugin  # 2
+        - object: B                             #3
+          fields:                               #4
+            foo:                                #5
+                WrongTypePlugin.abcdef: 5  #6
+        """
+        with pytest.raises(DataGenError) as e:
+            generate(StringIO(yaml))
+        assert 6 > e.value.line_num >= 3
+
+    def test_null_attributes(self):
+        yaml = """
+        - plugin: tests.test_custom_plugins_and_providers.WrongTypePlugin  # 2
+        - object: B                             #3
+          fields:                               #4
+            foo:                                #5
+                WrongTypePlugin.junk: 5  #6
         """
         with pytest.raises(DataGenError) as e:
             generate(StringIO(yaml))
