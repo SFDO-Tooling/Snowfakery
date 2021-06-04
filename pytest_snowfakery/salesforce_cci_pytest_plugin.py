d6fd18f8c3db013d242a2f1be91ff9883621072e
Paul Prescod <pprescod@salesforce.com>
2021-06-03 17:45:37 -0700

    Attempt to fix CI environment

--- pytest_snowfakery/salesforce_cci_pytest_plugin.py
+++ pytest_snowfakery/salesforce_cci_pytest_plugin.py
@@ -1,27 +1,32 @@
 from pytest import fixture
 
 
-try:
-    from cumulusci.tests.pytest_plugins.pytest_sf_orgconnect import (
-        pytest_addoption,
-        sf_pytest_orgname,
-        org_config,
-        runtime,
-        sf,
-        project_config,
-    )
-
-except ImportError:
-    print("Salesforce features will not be tested")
-    pytest_sf_orgconnect = None
-
-    def sf_pytest_orgname(request):
-        return "OFFLINE_NO_ORG"
+from cumulusci.tests.pytest_plugins.pytest_sf_orgconnect import (
+    pytest_addoption,
+    sf_pytest_orgname,
+    org_config,
+    sf,
+    project_config,
+)
+from cumulusci.cli.runtime import CliRuntime
+from cumulusci.core.runtime import BaseCumulusCI
 
 
 @fixture(scope="session")
 def orgname(request):
     return sf_pytest_orgname(request) or "orgname"
+
+
+@fixture(scope="session")
+def runtime(request):
+    """Get the CumulusCI runtime for the current working directory."""
+
+    # if there is a real orgname, use a real CliRuntime
+    # to get access to it
+    if sf_pytest_orgname(request):
+        return CliRuntime()
+    else:
+        return BaseCumulusCI()
 
 
 __all__ = [
