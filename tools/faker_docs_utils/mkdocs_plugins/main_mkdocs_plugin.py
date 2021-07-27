f48fb8099a9173d4f79ab4cd0b06b95847407e88
Paul Prescod <pprescod@salesforce.com>
2021-07-27 02:04:32 -0700

    Case insensitive comparison.

--- tools/faker_docs_utils/mkdocs_plugins/main_mkdocs_plugin.py
+++ tools/faker_docs_utils/mkdocs_plugins/main_mkdocs_plugin.py
@@ -58,7 +58,7 @@ class Plugin(BasePlugin):
             build_locales_env = os.environ.get(
                 "SF_MKDOCS_BUILD_LOCALES"
             ) or self.config.get("build_locales", None)
-            if build_locales_env == "False":
+            if build_locales_env.lower() == "false":
                 locales_list = ["en_US", "fr_FR"]
             elif build_locales_env in (True, "True", None):
                 locales_list = None  # means "all"
