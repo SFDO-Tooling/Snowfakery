81d648d686f32e8be758c7123a07e9f66692eb8f
Paul Prescod <pprescod@salesforce.com>
2021-11-28 10:09:59 -0800

    Allow zero-prefixed strings of digits

--- tests/test_types.py
+++ tests/test_types.py
@@ -4,7 +4,7 @@ from io import StringIO
 from snowfakery.data_generator import generate
 
 
-class TestGenerateMapping:
+class TestTypes:
     @mock.patch("snowfakery.output_streams.DebugOutputStream.write_row")
     def test_empty_string(self, write_row):
         yaml = """
@@ -13,4 +13,15 @@ class TestGenerateMapping:
                 bar: ""
         """
         generate(StringIO(yaml))
-        print(write_row.mock_calls[0][1][1]["bar"] == "")
+        assert write_row.mock_calls[0][1][1]["bar"] == ""
+
+    def test_zero_prefixed_string(self, generated_rows):
+        yaml = """
+            - object: Foo
+              fields:
+                bar: "012345"
+                bar2: ${{"012345"}}
+        """
+        generate(StringIO(yaml))
+        assert generated_rows.row_values(0, "bar") == "012345"
+        assert generated_rows.row_values(0, "bar2") == "012345"
