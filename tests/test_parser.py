01f1f2e6cf2af25c7a37f45a3177cf8a4b2a1196
Paul Prescod <pprescod@salesforce.com>
2021-12-21 11:31:29 -0800

    Improve error message of an unusual corner case

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,6 +5,7 @@ from datetime import date
 import pytest
 
 from snowfakery.parse_recipe_yaml import parse_recipe
+from snowfakery import data_gen_exceptions as exc
 
 
 class TestParseGenerator:
@@ -40,4 +41,18 @@ class TestParseGenerator:
           nickname: Foo.bar
         """
         with pytest.warns(UserWarning, match="Foo.bar.*nickname.*"):
+            parse_recipe(StringIO(yamlstr))
+
+    def test_parse_var_name_missing(self):
+        yamlstr = """
+- object: Outside
+  fields:
+    value: ${{the_var}}
+    __the_other_var: 20
+    the_other_value: ${{__the_other_var}}
+  friends:
+    - var:
+      value: 12
+        """
+        with pytest.raises(exc.DataGenSyntaxError):
             parse_recipe(StringIO(yamlstr))
