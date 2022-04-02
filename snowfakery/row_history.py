a8431dad298d7af6165930f6b795e07ce18863fe
Paul Prescod <pprescod@salesforce.com>
2022-04-01 17:04:31 -0700

    WIP: deep references using SQL Lite

--- snowfakery/row_history.py
+++ snowfakery/row_history.py
@@ -1,54 +1,28 @@
-from sqlalchemy import create_engine, Table
-from sqlalchemy.engine import Engine
-
-from sqlalchemy import (
-    Table,
-    Column,
-    Integer,
-    String,
-    MetaData,
-    insert,
-    Index,
-    JSON,
-    PickleType,
-    UniqueConstraint,
-)
-
-from snowfakery.plugins import ObjectRow
+import sqlite3
 
 
 class RowHistory:
     def __init__(self):
-        self.db = create_engine("sqlite://")
-        self.table = _make_history_table(self.db)
-        self.connection = self.db.connect()
-        self.insert = insert(self.table)
+        self.conn = _make_history_table(":memory:")
 
     def write_row(self, tablename: str, nickname: str, row: dict):
-        stmt = self.insert.values(
-            id=row["id"], table=tablename, nickname=nickname, data=str(row)
+        self.conn.execute(
+            "INSERT INTO snowfakery_history VALUES (?, ?, ?, ?)",
+            (
+                str(id),
+                tablename,
+                nickname,
+                repr(row),
+            ),
         )
-        # TODO: experiment with commit-less operatrion and its performance implications
-        # with self.connection.begin():
-        self.connection.execute(stmt)
 
 
-def _make_history_table(db: Engine) -> Table:
-    metadata = MetaData(bind=db)
-    history_table = Table(
-        "snowfakery_history",
-        metadata,
-        Column("id", Integer, nullable=False),
-        Column("table", String(30), nullable=False),
-        Column("nickname", String),
-        Column(
-            "data", String, nullable=True
-        ),  # TODO: experiment with type string, JSON
-        # Index("identifier", "id", "table", unique=True),
-        # UniqueConstraint(
-        #     "id",
-        #     "table",
-        # ),
+def _make_history_table(dbname):
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    c.execute("DROP TABLE IF EXISTS snowfakery_history;")
+    c.execute(
+        "CREATE TABLE snowfakery_history (id INTEGER NOT NULL, tablename VARCHAR(255) NOT NULL, nickname VARCHAR(255) , data VARCHAR NOT NULL)"
     )
-    metadata.create_all()
-    return history_table
+    conn.commit()
+    return conn
