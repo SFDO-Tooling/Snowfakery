f94b60b7b765e426d5e585be09405800f9b97919
Paul Prescod <pprescod@salesforce.com>
2022-06-15 14:30:10 -0700

    Fix inter-object references

--- tests/test_DataPack.py
+++ tests/test_DataPack.py
@@ -3,7 +3,11 @@ from unittest.mock import patch
 
 from snowfakery.data_generator import generate
 from snowfakery.data_generator_runtime import StoppingCriteria
-from snowfakery.experimental.DataPack import DataPack, ApexDataPack
+from snowfakery.experimental.DataPack import (
+    DataPack,
+    ApexDataPack,
+    SalesforceCompositeAPIOutput,
+)
 import json
 
 ## Fill this out when it isn't experimental anymore
@@ -25,6 +29,19 @@ class TestSalesforceCompositeAPIOutput:
         single_payload = json.loads(data["data"][0])
         print(single_payload)
         assert single_payload["graphs"][0]["compositeRequest"][0]["method"] == "POST"
+
+    def test_reference(self):
+        out = StringIO()
+        output_stream = SalesforceCompositeAPIOutput(out)
+        with open("examples/basic-salesforce.recipe.yml") as f:
+            generate(f, {}, output_stream)
+        output_stream.close()
+        print(out.getvalue())
+        data = json.loads(out.getvalue())
+        assert (
+            data["graphs"][0]["compositeRequest"][-1]["body"]["AccountId"]
+            == "@{Account_2.id}"
+        )
 
     @patch("snowfakery.experimental.DataPack.MAX_BATCH_SIZE", 50)
     def test_composite_upsert(self):
