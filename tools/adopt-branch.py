3fa8b6e658a37bb87f9dd1c5566d25699c4ce5fe
Paul Prescod <pprescod@salesforce.com>
2023-05-12 10:46:00 -0700

    Go from a PR, not a URL.

--- tools/adopt-branch.py
+++ tools/adopt-branch.py
@@ -2,12 +2,23 @@
 import json
 import os
 import subprocess
+import typing
 
 import click
 
 URL_RE = (
     "(?P<repo_root>https://github.com/(?P<owner>.+)/(?P<repo>.+))/tree/(?P<branch>.+)"
 )
+
+
+class PRInfo(typing.NamedTuple):
+    owner: str
+    repo: str
+    branch: str
+    repo_url: str
+    title: str
+    body: str
+    author: str
 
 
 def command(cmd):
@@ -28,7 +39,7 @@ def pr_to_branch(pr_number):
             "view",
             str(pr_number),
             "--json",
-            "url,headRepositoryOwner,headRepository,headRefName",
+            "url,headRepositoryOwner,headRepository,headRefName,title,body,author",
         ],
         capture_output=True,
         text=True,  # This is to ensure that the output from the subprocess is text
@@ -42,11 +53,18 @@ def pr_to_branch(pr_number):
     repo = pr_info["headRepository"]["name"]
     branch = pr_info["headRefName"]
     print(pr_info["headRepository"])
-    repo_url = f"https://github.com/{owner}/{repo}"
 
-    assert all((owner, repo, branch, repo_url)), (owner, repo, branch, repo_url)
+    assert all((owner, repo, branch)), (owner, repo, branch)
 
-    return owner, repo, branch, repo_url
+    return PRInfo(
+        owner,
+        repo,
+        branch,
+        f"https://github.com/{owner}/{repo}",
+        pr_info["title"],
+        pr_info["title"],
+        pr_info["author"],
+    )
 
 
 @click.command()
@@ -57,17 +75,18 @@ def adopt_branch(pr_number):
 
     pr_number is a number like "553" or whatever.
     """
-    owner, repo, branch, repo_url = pr_to_branch(pr_number)
-    assert all((owner, repo, branch, repo_url)), (owner, repo, branch, repo_url)
-    print("Using", repo, branch)
+    pr_info = pr_to_branch(pr_number)
+    print("Using", pr_info.repo, pr_info.branch)
     try:
-        command(f"git remote show {owner} 2>/dev/null")
+        command(f"git remote show {pr_info.owner} 2>/dev/null")
     except AssertionError:
-        command(f"git remote add {owner} {repo_url}")
-    command(f"git fetch {owner}")
-    command(f"git checkout --track {owner}/{branch}")
-    command("git push origin")
-    command("gh pr create")
+        command(f"git remote add {pr_info.owner} {pr_info.repo_url}")
+    command(f"git fetch {pr_info.owner}")
+    command(f"git checkout --track {pr_info.owner}/{pr_info.branch}")
+    command("git push origin --set-upstream")
+    title = f"{pr_info.title} -- from {pr_info.author}"
+    body = f"Copied from #{[pr_number]} by {pr_info.author}"
+    subprocess.run(["gh", "pr", "create", "--title", title, "--body", body])
 
 
 adopt_branch()
